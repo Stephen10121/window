315f9eb121e6f5476bbdfad5db74492456176249
Stephen Gruzin <stephengrzn@gmail.com>
2025-09-04 15:10:45 -0700

    Forgot to reexport all the components and stuff

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
 // Reexport your entry components here
+export { default as WindowManager } from "./WindowManager.svelte";
+export { default as Window } from "./Window.svelte";
+
+export type { WindowDragConfig, ActualWindowProps } from "./utils.js";
+export { MouseContext, WindowContext, defaultWindowValues } from "./utils.js";
